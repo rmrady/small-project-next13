48c264f83e574aa28159e6b30b48658637d72647
razieh7 <rmrady758@gmail.com>
2024-03-06 08:55:32 +0330

    rote-parament

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,5 +1,9 @@
 async function getTickets(id){
-    const res = await fetch('http://localhost:4000/tickets/' +  id)
+    const res = await fetch('http://localhost:4000/tickets/' +  id,{
+      next: {
+        revalidate: 10
+      }
+    })
     return res.json()
 }
 
@@ -7,13 +11,19 @@ async function getTickets(id){
 export default async function TicketDetails({params}) {
     const ticket = await getTickets(params.id)
   return (
-    <div>
-        <p>
-        {ticket.body}
-        </p>
+    <div className="h-[50vh] mt-10">
+      <h2 className="font-bold text-[20px] py-3">{ticket.title}</h2>
+      <p>{ticket.body}</p>
+      <p className={`pill ${ticket.priority}`}>{ticket.priority}</p>
+      <p className="text-blue-400 font-bold pt-3">{ticket.user_email}</p>
     </div>
   )
 }
+
+
+
+
+
 
 
 
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,23 +1,29 @@
+async function getTickets() {
+  const res = await fetch("http://localhost:4000/tickets",{
+    next: {
+        revalidate: 10
+      }
+  })
 
-async function getTickets(){
-    const res = await fetch('http://localhost:4000/tickets')
-    return res.json()
+  
+  return res.json();
 }
 
-
 export default async function Page() {
-    const tickets = await getTickets()
+  const tickets = await getTickets();
   return (
     <div className=" pt-20">
-        <h2 className="text-purple-400 text-[20px] font-bold pt-10">Page Tickets</h2>
-        <p className="text-gray-500">New member of the web dev</p>
-        {tickets.map((val)=>(
-            <div  key={val.id} className="bg-white p-6 mt-7">
-                <h3 className="text-[19px] font-bold pb-6">{val.title}</h3>
-                <p className="text-gray-500">{val.body.slice(0,200)}...</p>
-                <button className="bg-green-300 px-5 py-1 mt-5">see morn</button>
-            </div>
-        ))}
+      <h2 className="text-purple-400 text-[20px] font-bold pt-10">
+        Page Tickets
+      </h2>
+      <p className="text-gray-500">New member of the web dev</p>
+      {tickets.map((val) => (
+        <div key={val.id} className="bg-white p-6 mt-7">
+          <h3 className="text-[19px] font-bold pb-6">{val.title}</h3>
+          <p className="text-gray-500">{val.body.slice(0, 200)}...</p>
+          <button className="bg-green-300 px-5 py-1 mt-5">see morn</button>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
