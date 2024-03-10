af863753e0cf5fd995521644a04c1b142a3425b0
razieh7 <rmrady758@gmail.com>
2024-03-10 18:03:21 +0330

    verify

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,17 +1,40 @@
 "use client"
 
+import { useState } from "react"
+import { useRouter } from "next/navigation";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import AuthForm from "../AuthForm"
 
 
 export default function Signup() {
+  
+  const router = useRouter()
+  const [formError, setFormError] = useState('');
+
   const handleSubmit = async(e, email, password) =>{
     e.preventDefault()
-    console.log('user login', email, password);
+    const supabase = createClientComponentClient()
+    const {error} =await supabase.auth.SignUp({
+      email,
+      password,
+      options:{
+        emailRedirectTo: `${location.origin}/api/auth/callback`
+      }
+    })
+    if(error){
+      setFormError(error.message)
+    }
+    if(!error){
+      router.push('/verify')
+    }
   }
   return (
     <main className="w-full flex flex-col items-center">
       <h2 className="text-[20px]  text-purple-500  mb-5">Signup</h2>
       <AuthForm handleSubmit={handleSubmit}/>
+      {formError && (
+        <div>{formError}</div>
+      )}
     </main>
   )
 }
---
+++ app/(auth)/verify/page.jsx
@@ -0,0 +1,10 @@
+
+
+export default function varify() {
+  return (
+    <main>
+        <h2>thanks for regiter</h2>
+        <p>Before Loggin, you need to verify your Email Adress.</p>
+    </main>
+  )
+}
