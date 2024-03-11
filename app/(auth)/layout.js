5af7108ac813e5f55e474d37bda34ec8347ca568
razieh7 <rmrady758@gmail.com>
2024-03-11 19:11:55 +0330

    finish-supabase

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,5 +1,18 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from 'next/headers'
+import { redirect } from "next/navigation";
 import Link from 'next/link'
-export default function AuthLayout({children}) {
+
+
+export default async function AuthLayout({children}) {
+
+  const supabase = createServerComponentClient({cookies});
+  const { data } = await supabase.auth.getSession()
+
+  if( data.session){
+    redirect('/')
+  }
+
   return (
     <>
         <nav>
